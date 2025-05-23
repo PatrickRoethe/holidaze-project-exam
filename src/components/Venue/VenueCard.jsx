728668b1ba13d636cf96317365382df3d8bbb06c
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2025-05-24 00:13:06 +0200

    Update VenueCard.jsx
    link to venue details

--- src/components/Venue/VenueCard.jsx
+++ src/components/Venue/VenueCard.jsx
@@ -1,20 +1,23 @@
+import { Link } from "react-router-dom";
 import Card from "../base/Card";
 
 export default function VenueCard({ venue }) {
-  const { name, media, location, price } = venue;
+  const { name, media, location, price, id } = venue;
 
   return (
-    <Card className="hover:shadow-2xl hover:scale-[1.02] transition-all duration-200 ease-in-out">
-      <img
-        src={media[0]?.url || "/placeholder.jpg"}
-        alt={name}
-        className="w-full h-48 object-cover rounded"
-      />
-      <h2 className="text-lg font-semibold mt-2 line-clamp-1">{name}</h2>
-      <p className="text-sm text-gray-600">
-        {location?.city || "Unknown"}, {location?.country || "Unknown"}
-      </p>
-      <p className="text-primary font-bold">{price} NOK / night</p>
-    </Card>
+    <Link to={`/venues/${id}`}>
+      <Card className="hover:shadow-2xl hover:scale-[1.02] transition-all duration-200 ease-in-out">
+        <img
+          src={media[0]?.url || "/placeholder.jpg"}
+          alt={name}
+          className="w-full h-48 object-cover rounded"
+        />
+        <h2 className="text-lg font-semibold mt-2 line-clamp-1">{name}</h2>
+        <p className="text-sm text-gray-600">
+          {location?.city || "Unknown"}, {location?.country || "Unknown"}
+        </p>
+        <p className="text-primary font-bold">{price} NOK / night</p>
+      </Card>
+    </Link>
   );
 }
